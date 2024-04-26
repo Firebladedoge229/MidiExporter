46b4bc5a4e5c96a3062ccb616ede8142ae8ad0a8
Fireblade <72758695+Firebladedoge229@users.noreply.github.com>
2024-04-26 16:48:27 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# MidiExporter
+![Midi Exporter](https://github.com/Firebladedoge229/MidiExporter/blob/main/midiexporter.png?raw=true)
+
+# Midi Exporter
+
+A exporter that utilizes MIDI to MP3 conversion.
+
+## Installation
+
+Simply run the executable found at the [Releases](https://github.com/Firebladedoge229/MidiExporter/releases/latest) page.
+
+A [Windows Defender SmartScreen](https://learn.microsoft.com/en-us/windows/security/operating-system-security/virus-and-threat-protection/microsoft-defender-smartscreen/) window may display, or your anti-virus might trigger, this is due to the application signing system by [PyInstaller](https://github.com/pyinstaller/pyinstaller).
+
+If you are suspicious, feel free to compile the code yourself!
+
+![Showcase](https://github.com/Firebladedoge229/MidiExporter/assets/72758695/c203d502-0aa9-4078-a647-42d83e713318)
+
+## Requirements
+
+[FFmpeg](https://ffmpeg.org/download.html) - WAV to MP3\
+[FluidSynth](https://github.com/FluidSynth/fluidsynth/releases/latest) - MIDI to WAV
+
+## Author
+
+[Firebladedoge229](https://www.github.com/Firebladedoge229) - Creator
+
+## Credits 
+
+[SoftIcons](https://www.softicons.com/system-icons/toyfactory-icons-by-mira/file-midi-icon) - MIDI Icon
---
+++ midiexporter.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import sv_ttk
+import secrets
+import subprocess
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+
+if getattr(sys, 'frozen', False):
+    application_path = sys._MEIPASS
+else:
+    application_path = os.path.dirname(os.path.abspath(__file__))
+
+def select_soundfont():
+    soundfont_path = filedialog.askopenfilename(title="Select SoundFont file", filetypes=[("SoundFont files", "*.sf2")])
+    soundfont_entry.delete(0, tk.END)
+    soundfont_entry.insert(tk.END, soundfont_path)
+
+def select_midi():
+    midi_path = filedialog.askopenfilename(title="Select MIDI file", filetypes=[("MIDI files", "*.mid")])
+    midi_entry.delete(0, tk.END)
+    midi_entry.insert(tk.END, midi_path)
+
+def convert_to_mp3():
+    soundfont_path = soundfont_entry.get()
+    midi_path = midi_entry.get()
+    output_path = filedialog.asksaveasfilename(defaultextension=".mp3", filetypes=[("MP3 files", "*.mp3")])
+
+    if soundfont_path and midi_path and output_path:
+        token = secrets.token_urlsafe(16)
+
+        subprocess.Popen(f'fluidsynth -F "%temp%\\{token}.wav" -ni "{soundfont_path}" "{midi_path}"', shell = True).wait()
+        subprocess.Popen(f'ffmpeg -i "%temp%\\{token}.wav" -acodec mp3 "{output_path}"', shell = True).wait()
+        subprocess.Popen(f'del %temp%\\{token}.wav', shell = True).wait()
+    else:
+        status_label.config(text="Please select SoundFont, MIDI, and output path.")
+
+root = tk.Tk()
+root.iconbitmap(application_path + "\\midi.ico")
+root.title("Fire's MIDI to MP3 Exporter")
+
+ttk.Label(root, text="Select SoundFont file:").grid(row=0, column=0, padx=5, pady=5)
+soundfont_entry = ttk.Entry(root, width=50)
+soundfont_entry.grid(row=0, column=1, padx=5, pady=40)
+soundfont_button = ttk.Button(root, text="Browse", command=select_soundfont)
+soundfont_button.grid(row=0, column=2, padx=5, pady=5)
+
+ttk.Label(root, text="Select MIDI file:").grid(row=1, column=0, padx=5, pady=5)
+midi_entry = ttk.Entry(root, width=50)
+midi_entry.grid(row=1, column=1, padx=5, pady=5)
+midi_button = ttk.Button(root, text="Browse", command=select_midi)
+midi_button.grid(row=1, column=2, padx=5, pady=5)
+
+convert_button = ttk.Button(root, text="Convert to MP3", command=convert_to_mp3)
+convert_button.grid(row=2, column=1, padx=5, pady=5)
+
+status_label = ttk.Label(root, text="")
+status_label.grid(row=3, column=1, padx=5, pady=15)
+
+sv_ttk.set_theme("dark")
+
+root.resizable(False, False)
+root.mainloop()
